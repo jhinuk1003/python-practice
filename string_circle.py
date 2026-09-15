c9bd1c2e50b11fa931dd573ff7ed76775adbd3bd
Jhinuk Roy <jhinukr64@gmail.com>
2026-09-15 20:29:29 +0530

    Add files via upload

---
+++ string_circle.py
@@ -0,0 +1,5 @@
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = 'The area of a circle with a radius {} is {:.2f}.'.format(radius, area) # 2 digits after decimal
+print(formated_string)
